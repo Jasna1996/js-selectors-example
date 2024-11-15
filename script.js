8c10a3812bac4e8e67d541f54f516b6ec99ed89d
Jasna <jasnajaleel1996@gmail.com>
2024-11-15 11:48:42 +0530

    added HTML and Js for selectors

---
+++ script.js
@@ -0,0 +1,23 @@
+const mainTitle = document.getElementById('main-title');
+console.log(mainTitle)
+
+const containers = document.getElementsByClassName('container');
+console.log(containers)
+
+const sections = document.getElementsByClassName('section')
+console.log(sections)
+
+const paras = document.getElementsByTagName('p');
+console.log(paras)
+
+const firstbtn = document.querySelector('.btn')
+console.log(firstbtn)
+
+const firstsection = document.querySelector('.section')
+console.log(firstsection)
+
+const allBtns = document.querySelectorAll('.btn');
+console.log(allBtns)
+
+const allParas = document.querySelectorAll('.container p');
+console.log(allParas)
